912d5f7b504c1dca8dafbf9e77fcbc9a8b893937
Rafael Grande <rafael@grande.com>
2023-08-19 00:23:57 -0300

    Insert font in title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 
 import List from './components/List'
 import styles from './styles/Page.module.scss'
+import localFont from 'next/font/local'
+
+const myFont = localFont({ src: './fonts/HarryP.ttf' })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      teste
+      <div className={`${myFont.className} ${styles.main__title}`}>Persons Harry Potter</div>
       <List />
 
     </main>
